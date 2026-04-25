df18ce8be26d488c08cc42dbda996cfbe329d772
dylzen <dytangredi@gmail.com>
2026-04-25 15:53:04 +0200

    remove not working references

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import ac_data
 import mal_data
-import popculture
 
 
 USER_CHOICE = """
@@ -8,7 +7,6 @@ Choose an option:
 - 'a' : AC - get basic metadata and next volumes dates
 - 'm' : MAL - get ratings, popularity and rank
 - 'b' : Fetches from both services, then quits
-- 'p' : Search for manga (user input) and get volumes' links, then quits
 - 'q' : QUIT
 
 Choice: """
@@ -18,7 +16,7 @@ def menu():
     while user_input != 'q':
         if user_input == 'a':
             ac_data.ac_write_to_xlsx(user_input)
-            
+
         elif user_input == 'm':
             mal_data.mal_write_to_xlsx(user_input)
         elif user_input == 'b':
@@ -27,9 +25,6 @@ def menu():
             ac_data.ac_write_to_xlsx(user_input)
             user_input = 'm'
             mal_data.mal_write_to_xlsx(user_input)
-            quit()
-        elif user_input == 'p':
-            popculture.search_manga()
             quit()
         user_input = input(USER_CHOICE)
 
